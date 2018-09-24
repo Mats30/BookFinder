3ca7524c2e3eb9180085075b57eb8f436e77935c
Mateusz Rataj <Mateusz_Rataj@epam.com>
2018-09-24 11:23:02 +0200

    Added:
    * new JAXB dependencies (resolved error with marshalling problem)

--- scrapper/src/main/java/controllers/BookController.java
+++ scrapper/src/main/java/controllers/BookController.java
@@ -3,9 +3,9 @@ package controllers;
 import model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.BookService;
@@ -21,7 +21,7 @@ public class BookController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public PagedResources<Resource<Book>> findScrappedBooks(final Pageable pageable) {
         return assembler.toResource(service.findPaginated(pageable));
     }
